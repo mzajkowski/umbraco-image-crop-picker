d61196041f9eb056a45268fcf5b9796a147190b0
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-05-08 16:49:05 +0200

    ci: added nuget build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,97 +1,128 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  // load the package JSON file
+  var pkg = grunt.file.readJSON('package.json');
 
-    var path = require('path');
+  // paths
+  var sourceDir = 'src/';
+  var tmpDir = 'tmp/';
+  var releaseDir = 'release/';
+  var projectDir = sourceDir + pkg.name + '/';
+  var releaseFilesDir = releaseDir + 'files/';
+  var tmpDirRelease = tmpDir + releaseFilesDir;
 
-    // load the package JSON file
-    var pkg = grunt.file.readJSON('package.json');
+  // load information about the assembly
+  var assembly = grunt.file.readJSON(projectDir + 'Properties/AssemblyInfo.json');
 
-    // get the project paths
-    var sourceDir = 'src/',
-        projectDir = sourceDir + pkg.name + '/';
+  // get the version of the package
+  var version = assembly.informationalVersion ? assembly.informationalVersion : assembly.version;
 
-    // get the release paths
-    var releaseDir = 'release/',
-        releaseFilesDir = releaseDir + 'files/';
+  var nuspecFileName = pkg.name + '.nuspec';
 
-    // load information about the assembly
-    var assembly = grunt.file.readJSON(projectDir + 'Properties/AssemblyInfo.json');
+  var buildedNuget = {};
 
-    // get the version of the package
-    var version = assembly.informationalVersion ? assembly.informationalVersion : assembly.version;
+  buildedNuget[`${tmpDir}${sourceDir}${pkg.name}/${nuspecFileName}`] = [projectDir + nuspecFileName]
 
-    grunt.initConfig({
-        pkg: pkg,
-        clean: {
+  grunt.template.addDelimiters('handlebars-like-delimiters', '{{', '}}');
+  grunt.initConfig({
+    pkg: pkg,
+    clean: {
+      release: ['release'],
+      tmp: ['tmp'],
+    },
+    copy: {
+      bacon: {
+        files: [
+          {
+            expand: true,
+            cwd: projectDir + 'obj/Release/',
+            src: [pkg.name + '.dll', pkg.name + '.xml'],
+            dest: releaseFilesDir + 'bin/',
+          },
+          {
+            expand: true,
+            cwd: projectDir + 'Web/UI/',
+            src: ['**'],
+            dest: releaseFilesDir,
+          },
+        ],
+      },
+      nuget: {
+          files: [
+              {
+                  src: [ sourceDir + '**/*.*'],
+                  dest: tmpDir
+              }
+          ]
+      }
+    },
+    template: {
+      nuspec: {
+        options: {
+          data: {
+            id: pkg.name,
+            name: pkg.name,
+            title: pkg.name,
+            version: pkg.version,
+            description: pkg.readme,
+            author: pkg.author.name,
+            copyright: assembly.copyright,
             files: [
-                releaseFilesDir + '**/*.*'
-            ]
+              {
+                path: tmpDir,
+                target: sourceDir + '**/*.*',
+              },
+            ],
+          },
+          delimiters: 'handlebars-like-delimiters',
         },
-        copy: {
-            bacon: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: projectDir + 'obj/Release/',
-                        src: [
-                            pkg.name + '.dll',
-                            pkg.name + '.xml'
-                        ],
-                        dest: releaseFilesDir + 'bin/'
-                    },
-                    {
-                        expand: true,
-                        cwd: projectDir + 'Web/UI/',
-                        src: ['**'],
-                        dest: releaseFilesDir
-                    }
-                ]
-            }
+        files: [
+          buildedNuget
+        ],
+      },
+    },
+    zip: {
+      release: {
+        cwd: releaseFilesDir,
+        src: [releaseFilesDir + '**/*.*'],
+        dest: releaseDir + 'zip/' + pkg.name + '.v' + version + '.zip',
+      },
+    },
+    umbracoPackage: {
+      dist: {
+        src: releaseFilesDir,
+        dest: releaseDir + 'umbraco/',
+        options: {
+          name: pkg.name,
+          version: version,
+          url: pkg.url,
+          license: pkg.license.name,
+          licenseUrl: pkg.license.url,
+          author: pkg.author.name,
+          authorUrl: pkg.author.url,
+          readme: pkg.readme,
+          outputName: pkg.name + '.v' + version + '.zip',
         },
-        zip: {
-            release: {
-                cwd: releaseFilesDir,
-                src: [
-                    releaseFilesDir + '**/*.*'
-                ],
-                dest: releaseDir + 'zip/' + pkg.name + '.v' + version + '.zip'
-            }
+      },
+    },
+    nugetpack: {
+      dist: {
+        src: tmpDir + sourceDir + pkg.name + '/' + pkg.name + '.csproj',
+        dest: releaseDir + 'nuget/',
+        options: {
+          properties: 'Platform=AnyCPU;Configuration=Release',
         },
-        umbracoPackage: {
-            dist: {
-                src: releaseFilesDir,
-                dest: releaseDir + 'umbraco/',
-                options: {
-                    name: pkg.name,
-                    version: version,
-                    url: pkg.url,
-                    license: pkg.license.name,
-                    licenseUrl: pkg.license.url,
-                    author: pkg.author.name,
-                    authorUrl: pkg.author.url,
-                    readme: pkg.readme,
-                    outputName: pkg.name + '.v' + version + '.zip'
-                }
-            }
-        },
-        nugetpack: {
-            dist: {
-                src: projectDir + pkg.name + '.csproj',
-                dest: releaseDir + 'nuget/',
-                options: {
-                    properties: 'Platform=AnyCPU;Configuration=Release'
-                }
-            }
-        }
-    });
+      },
+    },
+  });
 
-    grunt.loadNpmTasks('grunt-umbraco-package');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-nuget');
-    grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-umbraco-package');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-nuget');
+  grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-template');
 
-    grunt.registerTask('dev', ['copy', 'zip', 'umbracoPackage', 'nugetpack']);
+    grunt.registerTask('dev', ['clean', 'copy', 'template', 'zip', 'umbracoPackage', 'nugetpack']);
 
-    grunt.registerTask('default', ['dev']);
-
+  grunt.registerTask('default', ['dev']);
 };
